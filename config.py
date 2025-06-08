47ed4c77289366dcc40f1423a77fff96f0299828
Eimo384 <eimoedueimo@gmail.com>
2025-06-09 02:35:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-BOT_TOKEN=7616918699:AAFS_xjwhO3yOI_J-PIpEXT816YcPrchI1M
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN not found in .env file")
