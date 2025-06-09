20f174e2da0ff8b7a67e5bd2bdcac7d77bac73a3
Eimo384 <eimoedueimo@gmail.com>
2025-06-10 01:48:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from commands.stats import stats_command
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 from config import BOT_TOKEN
@@ -12,6 +13,7 @@ def main():
     # Register callback query handlers
     app.add_handler(CallbackQueryHandler(pong_response, pattern="^ping_response$"))
     app.add_handler(CallbackQueryHandler(back_to_main, pattern="^back_to_main$"))
+    app.add_handler(CallbackQueryHandler(stats_command, pattern="^stats_response$"))
 
     # Start the bot
     print("Bot is fully loaded and ready. Awaiting launch.")
