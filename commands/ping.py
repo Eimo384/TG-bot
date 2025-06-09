041b3dbcc02041eca793835ff3452a0cba6938d0
Eimo384 <eimoedueimo@gmail.com>
2025-06-10 01:27:22 +0300

    Update ping.py

--- commands/ping.py
+++ commands/ping.py
@@ -1,1 +1,24 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackContext
+from utils.helpers import update_stat
 
+def ping_command(update: Update, context: CallbackContext) -> None:
+    update_stat("ping")
+
+    keyboard = [
+        [InlineKeyboardButton("🏓 Pong", callback_data='pong_response')],
+        [InlineKeyboardButton("🔙 Back", callback_data='back_to_main')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text("Ping received! Choose an action:", reply_markup=reply_markup)
+
+def handle_ping_callback(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    data = query.data
+    query.answer()
+
+    if data == "pong_response":
+        query.edit_message_text("🏓 Pong! That was fast.")
+    elif data == "back_to_main":
+        query.edit_message_text("🔙 Returning to main menu... (not yet implemented)")
