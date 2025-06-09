b93ecddbec92055092c5353482145cbe9b90cba8
Eimo384 <eimoedueimo@gmail.com>
2025-06-10 00:58:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,1 @@
-from config import BOT_TOKEN
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! I'm alive.")
-
-if __name__ == "__main__":
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
